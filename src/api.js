69b84a534295c0d1cb8ccd6864aa866668ef61f7
Mhmdzein1 <mohammad_zein_2000@hotmail.com>
2024-07-08 12:18:24 +0300

    initial commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "d1913056ccmshe221867d18ae050p17573djsn3b5020995f0e",
+    "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "a21211c93b6ec8d492ddd3b5b37cceac";
